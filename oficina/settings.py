9bd7c29b9e9d96d984a7564c8b34c397abdac773
willianjonhy05 <willianjohny05@gmail.com>
2024-03-04 21:38:05 -0300

    Alterações no Settings

--- oficina/settings.py
+++ oficina/settings.py
@@ -100,8 +100,12 @@ WSGI_APPLICATION = 'oficina.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
