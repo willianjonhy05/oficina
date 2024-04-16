3b82c6a68b3eedeed31c732f038f088cae4bd26f
willianjonhy05 <willianjohny05@gmail.com>
2024-04-15 21:25:25 -0300

    Novas Views

--- apps/servicos/views.py
+++ apps/servicos/views.py
@@ -6,34 +6,62 @@ from .models import Servico
 from servicos.models import Oficina
 
 # Create your views here.
+# @login_required
+# def novo_servico(request):
+#     template_name = 'servicos/novo_servico.html'
+#     context = {}
+#     usuario = request.user
+#     oficinas = Oficina.objects.filter(usuario=usuario)
+#     if request.method == 'POST':
+#         form = ServicoForm(request.POST)
+#         if form.is_valid():
+#             of = form.save(commit=False)
+#             oficina_id = request.POST.get('oficina_id') 
+#             oficina = Oficina.objects.get(id=oficina_id)
+#             of.oficina = oficina
+#             of.save()
+#             messages.success(request, 'Serviço cadastrado com sucesso!')
+#             return redirect('servicos:lista_servicos')
+#     else:
+#         form = ServicoForm()
+    
+#     context['form'] = form   
+#     context['oficinas'] = oficinas  
+#     return render(request, template_name, context)
+
 @login_required
 def novo_servico(request):
     template_name = 'servicos/novo_servico.html'
     context = {}
-    usuario = request.user
-    oficinas = Oficina.objects.filter(usuario=usuario)
     if request.method == 'POST':
         form = ServicoForm(request.POST)
+        oficina = get_object_or_404(Oficina, usuario=request.user)
         if form.is_valid():
-            of = form.save(commit=False)
-            oficina_id = request.POST.get('oficina_id') 
-            oficina = Oficina.objects.get(id=oficina_id)
-            of.oficina = oficina
-            of.save()
-            messages.success(request, 'Serviço cadastrado com sucesso!')
+            sf = form.save(commit=False)
+            sf.oficina = oficina
+            sf.save()
+            messages.success(request, "Serviço adicionado com sucesso!")
             return redirect('servicos:lista_servicos')
-    else:
-        form = ServicoForm()
-    
+    form = ServicoForm()
     context['form'] = form   
-    context['oficinas'] = oficinas  
     return render(request, template_name, context)
+
+# @login_required
+# def lista_servicos(request):
+#     template_name = 'servicos/lista_servicos.html' 
+#     oficinas = Oficina.objects.filter(usuario=request.user)         
+#     servicos = Servico.objects.filter(oficina__in=oficinas)
+#     context = {
+#         'servicos': servicos,
+#     }
+#     return render(request, template_name, context)
+
 
 @login_required
 def lista_servicos(request):
     template_name = 'servicos/lista_servicos.html' 
-    oficinas = Oficina.objects.filter(usuario=request.user)         
-    servicos = Servico.objects.filter(oficina__in=oficinas)
+    oficina = get_object_or_404(Oficina, usuario=request.user)
+    servicos = Servico.objects.filter(oficina=oficina)
     context = {
         'servicos': servicos,
     }
